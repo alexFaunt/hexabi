83ae22e2cc801139f3305de27a84bb55f3cbe33e
alexfaunt <alexfaunt@gmail.com>
2015-10-05 21:42:54 +0100

    Format correctly

--- src/components/Hex/Hex.js
+++ src/components/Hex/Hex.js
@@ -4,11 +4,10 @@ import React, { Component } from 'react';
 
 export default class Hex extends Component {
 
-  render() {
-    return <div className={styles.base} style={{
-      width: this.props.width,
-      marginLeft: this.props.marginLeft
-    }}/>;
-  }
-
+    render() {
+        return <div className={styles.base} style={{
+            width: this.props.width,
+            marginLeft: this.props.marginLeft
+        }}/>;
+    }
 };
