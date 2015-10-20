a6c40c8cee2352bd0d601f98c12c9dcc2cf0bf4e
alexfaunt <alexfaunt@gmail.com>
2015-10-20 23:42:26 +0100

    First model + bookshelf for postgres

--- example-server-config.js
+++ example-server-config.js
@@ -4,5 +4,13 @@ export default {
         secret: ''
     },
 
+    postgres: { // TODO - this can be done with a connString, and thats how old wasabi did it
+        host     : 'localhost',
+        user     : 'admin',
+        password : 'sa',
+        database : 'hexabi',
+        charset  : 'utf8'
+    },
+
     port: 8080
 }
